0dc3148adf8bf8b285402fcc210a763ae02893b4
ramlow07 <luamramlow@gmail.com>
2023-04-12 17:18:44 +0200

    att

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-const navLogo = document.querySelector('#navbar__logo');
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const navLogo = document.querySelector('#navbar__logo')
 
 // Display Mobile Menu
+
 const mobileMenu = () => {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-};
+   menu.classList.toggle('is-active')
+   menuLinks.classList.toggle('active') 
+}
 
 menu.addEventListener('click', mobileMenu);
