d2430826c65330cd412fe31b8fadf87bfa13094f
ramlow07 <luamramlow@gmail.com>
2023-04-16 22:58:07 +0200

    adicionando opcoes de aparelho, atualizando layout

--- app.js
+++ app.js
@@ -17,6 +17,45 @@ const highlightMenu = () => {
    const homeMenu = document.querySelector('#home-page')
    const aboutMenu = document.querySelector('#about-page')
    const servicesMenu = document.querySelector('#services-page')
-   let scrollPos = window.scrollY
-   
+   let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+   // adds "highlight" class to any menu items
+if(window.innerWidth > 960 && scrollPos < 680) {
+   homeMenu.classList.add('highlight')
+   aboutMenu.classList.remove('highlight')
+   return
+} else if (window.innerWidth > 960 && scrollPos < 1400) {
+   aboutMenu.classList.add('highlight')
+   homeMenu.classList.remove('highlight')
+   servicesMenu.classList.remove('highlight')
+   return
+}  else if (window.innerWidth > 960 && scrollPos < 2345) {
+   servicesMenu.classList.add('highlight')
+   aboutMenu.classList.remove('highlight')
+   return
 }
+
+if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+   elem.classList.remove('highlight')
+}
+
+}
+
+window.addEventListener('scroll', highlightMenu)
+window.addEventListener('click', highlightMenu)
+
+
+// close mobile menu when clicking on a menu item 
+
+const hideMobileMenu = () => {
+   const menuBars = document.querySelector('is-active')
+   if(window.innerWidth <= 768 && menuBars) {
+      menu.classList.toggle('is-active')
+      menuLinks.classList.remove('active')
+   }
+
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
